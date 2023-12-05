bd0c6548d3c99becc7c2fed785e5044e17674266
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2023-12-05 16:23:08 +0200

    Add project files.

---
+++ HW9_Anim/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+
+namespace HW9_Anim
+{
+    public partial class MainWindow : Window
+    {
+        private readonly Random rnd = new Random();
+        private readonly DispatcherTimer timer = new DispatcherTimer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DoubleAnimation animX = new DoubleAnimation();
+            animX.From = 0;
+            animX.To = 450;
+            animX.Duration = new Duration(TimeSpan.FromSeconds(2));
+            animX.RepeatBehavior = RepeatBehavior.Forever;
+            animX.AutoReverse = true;
+            TranslateTransform translateTransform = new TranslateTransform();
+            square.RenderTransform = translateTransform;
+            translateTransform.BeginAnimation(TranslateTransform.XProperty, animX);
+
+            timer.Interval = TimeSpan.FromSeconds(2);
+            timer.Tick += (s, e) =>
+            {
+                byte[] colorBytes = new byte[4];
+                rnd.NextBytes(colorBytes);
+                Color randomColor = Color.FromArgb(colorBytes[0], colorBytes[1], colorBytes[2], colorBytes[3]);
+
+                if (randomColor != Colors.Black)
+                {
+                    square.Fill = new SolidColorBrush(randomColor);
+                }
+            };
+            timer.Start();
+        }
+    }
+}
